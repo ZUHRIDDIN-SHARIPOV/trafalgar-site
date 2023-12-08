8c19b6543ff18765fa0e17cab2e12f56bb4334ab
ZUHRIDDIN-SHARIPOV <zuhriddinshx@gmail.com>
2023-12-08 14:43:49 +0500

    The services section of the site has been updated.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const body = document.querySelector("body");
 const headerMobile = document.querySelector(".site-header__mobile");
 const headerMobileItem = document.querySelectorAll(".site-header__mobile-item");
 const menuBtn = document.querySelector(".site-header__menu-logo");
@@ -5,12 +6,15 @@ const closeBtn = document.querySelector(".site-header__close-logo");
 
 menuBtn.onclick = () => {
   headerMobile.classList.add("site-header__mobile-key");
+  body.style.overflowY = "hidden";
 };
 closeBtn.onclick = () => {
   headerMobile.classList.remove("site-header__mobile-key");
+  body.style.overflowY = "scroll";
 };
 headerMobileItem.forEach((item) => {
   item.onclick = () => {
     headerMobile.classList.remove("site-header__mobile-key");
+    body.style.overflowY = "scroll";
   };
 });
